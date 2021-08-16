894d345b3cdaf05cc672ee75beb73b77399ac0fd
fxusyd <54692384+fxusyd@users.noreply.github.com>
2021-08-16 15:18:02 +1000

    Update and rename run.sh to entrypoint.sh
    set timezone, then run provided command in the same shell

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -x
 if [ ! -z $timezone ]; then
   ln -s /usr/share/zoneinfo/$timezone /etc/localtime
 else
   echo "timezone environment variable is not set"
 fi 
-java -jar  /JavaPrograms/CTP/Runner.jar & wait ${!}
+exec $@
